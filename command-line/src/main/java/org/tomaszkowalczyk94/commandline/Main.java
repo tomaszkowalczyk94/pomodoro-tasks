e708412a83e017e913c2067aac8ee4b415175b7d
tomaszkowalczyk94 <tomasz.kowalczyk94@gmail.com>
2020-10-31 23:25:47 +0100

    lanterna - simple tasks view

--- command-line/src/main/java/org/tomaszkowalczyk94/commandline/Main.java
+++ command-line/src/main/java/org/tomaszkowalczyk94/commandline/Main.java
@@ -1,8 +1,26 @@
 package org.tomaszkowalczyk94.commandline;
 
 
+import com.googlecode.lanterna.gui2.*;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello world");
+    public static void main(String[] args) throws IOException {
+        DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory();
+
+        try (Screen screen = defaultTerminalFactory.createScreen()) {
+            screen.startScreen();
+
+            final WindowBasedTextGUI textGui = new MultiWindowTextGUI(screen);
+            final Window window = new BasicWindow("Tasks");
+
+            Panel contentPanel = new TasksPanelBuilder().build(textGui);
+            window.setComponent(contentPanel);
+
+            textGui.addWindowAndWait(window);
+        }
     }
 }
